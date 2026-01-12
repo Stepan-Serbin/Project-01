e6f33c4299fe1787b5683017c114e7e2de0fc967
Stepan Serbin <stepanserbins@gmail.com>
2026-01-12 20:07:13 +0100

    Add new files and update existing ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
 import "./App.css";
 import Counter from "./components/Counter/Counter";
-import Goodbye from "./components/Goodbye/Goodbye"
-// import Greetings from "./components/Greetings/Greetings";
-import Tool from "./components/Tool/Tool";
+import GenderReveal from "./components/GenderReveal/GenderReveal";
+import Goodbye from "./components/Goodbye/Goodbye";
+import IdealWeightCalculator from "./components/IdealWeightCalculator/IdealWeightCalculator";
 import ProfileCard from "./components/ProfileCard/ProfileCard";
-
+import RandomDog from "./components/RandomDog/RandomDog";
+import SpaceMissionForm from "./components/SpaceMissionForm/SpaceMissionForm";
+import Tool from "./components/Tool/Tool";
 
 function App() {
+  const johnDawson = {
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7x7RFFT8-4WY26mVJxhk5lvmoTIhb_0NzAQ&s",
+    name: "John Dawson",
+    description: "Full Stack Dev",
+  };
+
   return (
     <>
-      <p>Hello, World!</p>
+      <p className="heading">Hello, World!</p>
       {/* props */}
       {/* <Greetings name="Alisher" />
       <Greetings name="Vadim" /> */}
 
-      <Tool />
+      <Goodbye familyName="Khamidov" score={90} />
+      <Goodbye familyName="Khamidov" />
 
-      <Goodbye familyName="Khamidov" score={90}/>
-      <Goodbye familyName="Khamidov"/>
-
+      <GenderReveal />
+      <RandomDog />
       <Counter />
-
-      <ProfileCard
-        avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7x7RFFT8-4WY26mVJxhk5lvmoTIhb_0NzAQ&s"
-        name="John Doe"
-        description="Frontend developer"
-      />
-
+      <Counter />
+      <SpaceMissionForm />
+      <Tool />
+      <IdealWeightCalculator />
+      <ProfileCard {...johnDawson} />
     </>
   );
 }
@@ -37,8 +44,8 @@ export default App;
 // Создайте компонент Goodbye
 // компонент должен возвращать параграф с текстом Goodbye, <family_name>
 
-// соответственно у компонента должен быть пропс familyName 
+// соответственно у компонента должен быть пропс familyName
 // Создайте два элемента с разными фамилиями
 
-// дополнительно добавьте score - числовой пропс 
+// дополнительно добавьте score - числовой пропс
 // Goodbye, John, your score is 97
